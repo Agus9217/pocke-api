d293a8afcc9c237e59b27b0cd2cfeded1ce729af
Agus9217 <justinibarrola92@gmail.com>
2022-10-05 01:33:37 -0300

    poke-api

--- main.js
+++ main.js
@@ -1,47 +1,65 @@
+const imgPrint = document.getElementById('card-img')
+const namePrint = document.getElementById('card-name')
+const hpPrint = document.getElementById('card-hp')
+const weightPrint = document.getElementById('card-weigth')
+const heightPrint = document.getElementById('card-heigth')
+const typePrint = document.getElementById('card-type')
+const abilityPrint = document.getElementById('ability')
+const attackPrint = document.getElementById('attack')
+const defencePrint = document.getElementById('defence')
+const speedPrint = document.getElementById('speed')
+const specialAttackPrint = document.getElementById('special-attack')
+const specialDefencePrint = document.getElementById('special-defence')
+const inputCapture = document.getElementById('pokemon')
+
 
 const fetchRequest = async (id) => {
   const URL = `https://pokeapi.co/api/v2/pokemon/${id}`
   const res = await fetch(URL)
   const data = await res.json()
-  console.log(data)
-  console.log('NOMBRE POKEMON ==>', data.name)
-  console.log('HABILIDAD 1 ==>',data.abilities[0].ability.name)
-  console.log('HABILIDAD 2 ==>',data.abilities[1].ability.name)
-  console.log('PESO ==>', data.weight / 10)
-  console.log('ALTURA ==>', data.height / 10)
-  console.log('IMAGEN ==>',data.sprites.other.home.front_default)
+  namePrint.innerHTML = `Name: ${data.name.toUpperCase()}`
+  abilityPrint.innerHTML = `${data.abilities[0].ability.name.toUpperCase()}, ${data.abilities[1].ability.name.toUpperCase()}`
+  weightPrint.innerHTML = `Weight: ${data.weight / 10} Kg`
+  heightPrint.innerHTML = `Height: ${data.height / 10} Mts`
+  imgPrint.src = `${data.sprites.other.home.front_default}`
   data.types.map((item) => {
-    console.log('TIPO ==>', item.type.name)
+    typePrint.innerHTML = `Type: ${item.type.name.toUpperCase()}`
   })
 
   const hpStat = data.stats[0]
-  console.log(hpStat.stat.name)
-  console.log(hpStat.base_stat)
-
+  hpPrint.innerHTML = `${hpStat.stat.name.toUpperCase()}: ${hpStat.base_stat}`
+  
+  
   const attackStat = data.stats[1]
-  console.log(attackStat.stat.name)
-  console.log(attackStat.base_stat)
+  attackPrint.innerHTML = `${attackStat.stat.name.toUpperCase()}: ${attackStat.base_stat}` 
+  
 
   const defenceStat = data.stats[2]
-  console.log(defenceStat.stat.name)
-  console.log(defenceStat.base_stat)
+  defencePrint.innerHTML = `${defenceStat.stat.name.toUpperCase()}: ${defenceStat.base_stat}`
+  
+
 
   const specialAttack = data.stats[3]
-  console.log(specialAttack.stat.name)
-  console.log(specialAttack.base_stat)
+  specialAttackPrint.innerHTML = `${specialAttack.stat.name.toUpperCase()}: ${specialAttack.base_stat}` 
 
   const specialDefence = data.stats[4]
+  specialDefencePrint.innerHTML = `${specialDefence.stat.name.toUpperCase()}: ${specialDefence.base_stat}`
   console.log(specialDefence.stat.name)
   console.log(specialDefence.base_stat)
 
   const speedStat = data.stats[5]
-  console.log(speedStat.stat.name)
-  console.log(speedStat.base_stat)
+  speedPrint.innerHTML = `${speedStat.stat.name.toUpperCase()}: ${speedStat.base_stat}`
+  
 
   return data
 }
 
+inputCapture.addEventListener('input', (e) => {
+  const result = e.target.value
+  fetchRequest(result)
+})
+
 
 // url species para capturar el color
 
-fetchRequest(25)
+
